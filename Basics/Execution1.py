97d590870e44064d647d1c6a93ef1a5b52794f9a
kamarajanis <kamarajkamarajanis@gmail.com>
2018-09-30 20:38:00 +0530

    second_code

--- Basics/Execution1.py
+++ Basics/Execution1.py
@@ -1,34 +1,14 @@
 Python 3.6.6 (v3.6.6:4cf1f54eb7, Jun 27 2018, 03:37:03) [MSC v.1900 64 bit (AMD64)] on win32
 Type "copyright", "credits" or "license()" for more information.
->>> print("Hello")
-Hello
->>> 
-
->>> x=4
->>> x
-4
->>> 
-
->>> print "Hello"
-SyntaxError: Missing parentheses in call to 'print'. Did you mean print("Hello")?
->>> print("Hello")
-Hello
->>> 
-if True
-SyntaxError: invalid syntax
->>> clear
-Traceback (most recent call last):
-  File "<pyshell#8>", line 1, in <module>
-    clear
-NameError: name 'clear' is not defined
->>> 
-=============== RESTART: I:/Learning/python-code/Basics/if.py ===============
-Yes
->>> 
-=============== RESTART: I:/Learning/python-code/Basics/if.py ===============
-Yes
 >>> 
 =============== RESTART: I:/Learning/python-code/Basics/if.py ===============
 Yes
 Error
+>>> 
+=============== RESTART: I:/Learning/python-code/Basics/if.py ===============
+Yes
+Error
+>>> 
+=============== RESTART: I:/Learning/python-code/Basics/if.py ===============
+Yes
 >>> 
---
+++ Basics/if.py
@@ -0,0 +1,5 @@
+if True:
+    print("Yes")
+else:
+    print("No")
+    
